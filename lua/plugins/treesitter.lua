f9596bea4698a652c83c585bcc6c636438953557
Mohammad Shammout <fdsmohammed@gmail.com>
2026-03-26 22:57:21 +0300

    Update plugins and fix treesitter config for new API

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,30 +1,16 @@
 return { -- Highlight, edit, and navigate code
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-	main = "nvim-treesitter.configs",
-	opts = {
-		ensure_installed = {
-			"vim",
-			"lua",
-			"luadoc",
-			"html",
-			"css",
-			"javascript",
-			"typescript",
-			"tsx",
-			"c",
-			"dart",
-			"markdown",
-			"markdown_inline",
-		},
-		auto_install = true,
-		highlight = {
-			enable = true,
-			use_languagetree = true,
-			additional_vim_regex_highlighting = { "ruby" },
-		},
-		indent = { enable = true, disable = { "ruby" } },
-	},
+	config = function()
+		local langs = { "vim", "lua", "luadoc", "html", "css", "javascript", "typescript", "tsx", "c", "dart", "markdown", "markdown_inline" }
+		local installed = require("nvim-treesitter").get_installed()
+		local to_install = vim.tbl_filter(function(lang)
+			return not vim.list_contains(installed, lang)
+		end, langs)
+		if #to_install > 0 then
+			require("nvim-treesitter").install(to_install)
+		end
+	end,
 	dependencies = {
 		{
 			"OXY2DEV/markview.nvim",
