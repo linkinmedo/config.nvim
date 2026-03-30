a4abb111b005e9eee7d1c3fd8d341a29b7e1d013
Mohammad Shammout <fdsmohammed@gmail.com>
2026-03-30 12:20:15 +0300

    fix treesitter config for legacy API and disable codecompanion luarocks build

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,16 +1,24 @@
 return { -- Highlight, edit, and navigate code
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-	config = function()
-		local langs = { "vim", "lua", "luadoc", "html", "css", "javascript", "typescript", "tsx", "c", "dart", "markdown", "markdown_inline" }
-		local installed = require("nvim-treesitter").get_installed()
-		local to_install = vim.tbl_filter(function(lang)
-			return not vim.list_contains(installed, lang)
-		end, langs)
-		if #to_install > 0 then
-			require("nvim-treesitter").install(to_install)
-		end
-	end,
+	opts = {
+		ensure_installed = {
+			"vim",
+			"lua",
+			"luadoc",
+			"html",
+			"css",
+			"javascript",
+			"typescript",
+			"tsx",
+			"c",
+			"dart",
+			"markdown",
+			"markdown_inline",
+		},
+		auto_install = true,
+		highlight = { enable = true },
+	},
 	dependencies = {
 		{
 			"OXY2DEV/markview.nvim",
